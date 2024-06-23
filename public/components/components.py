6abe1bcfceacdcc0c2ec173a42c106b7c2d55bc9
ahmed rakan <ar.aldhafeeri11@gmail.com>
2024-06-23 15:28:14 +0300

    splitting styled components into microfrontend

--- public/components/components.py
+++ public/components/components.py
@@ -2,39 +2,9 @@ import asyncio
 from public.loading import DOMIsReady
 from public.components.common import *
 from public.components.nav import *
-from public.components.sidebar import Sidebar
 from zenaura.client.component import Component
 from zenaura.client.mutator import mutator
 from public.styles import main_content, btn_one_class, with_theme_colors_text_no_hover
-from public.styled.menu import MenuExample
-from public.constants import menu_component_code
-
-def StyledComponents(active_comp, active_tab, context):
-  return Div("p-10", 
-    [
-      StyledComponentPresentation(
-         "Menu",
-         "Display a menu to the user - triggered by py-click",
-         "url",
-         MenuExample(context["open"]),
-         menu_component_code,
-         "menu",
-         active_comp, 
-         active_tab
-      ),
-      StyledComponentPresentation(
-         "Breadcrumb",
-         "Display hierarchical path to the current resurces - helps users identify their current location within web app. ",
-         "url",
-         Button(btn_one_class, "open", "null", "menu"),
-         menu_component_code,
-         "breadcrumb",
-         active_comp, 
-         active_tab
-      ),
-    ]
-  )
-
 
 class Components(Component):
   def __init__(self):
@@ -64,9 +34,5 @@ class Components(Component):
   def render(self):
     
     return  Div(main_content, [
-      # main content
-      Section([
-        Sidebar(),
-        StyledComponents(self.active_comp, self.active_tab, {"open": self.open_menu})
-      ], "grid grid-cols-[20%_80%]") if not self.loading else Loader()
+     MicroFrontend("url") if not self.loading else Loader()
     ])
